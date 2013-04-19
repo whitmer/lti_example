ad8d13c9692b49efd63ff529f8fc1eb98034e816
Brian Whitmer <brian@instructure.com>
2013-04-19 16:29:52 -0600

    more details on description field
    Change-Id: I98981dfcddf5dc4d6df5a7160289a81274801735
    Reviewed-on: https://gerrit.instructure.com/19872
    Reviewed-by: Brian Whitmer <brian@instructure.com>
    Product-Review: Brian Whitmer <brian@instructure.com>
    QA-Review: Brian Whitmer <brian@instructure.com>
    Tested-by: Brian Whitmer <brian@instructure.com>

--- handlebars/bin/templates.js
+++ handlebars/bin/templates.js
@@ -2245,7 +2245,7 @@ function program49(depth0,data) {
   stack1 = foundHelper || depth0.id;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "id", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "\"/>\n      </div>      \n    </div>\n    <div class=\"control-group\">\n      <label class=\"control-label\" for=\"description\">Description</label>\n      <div class=\"controls\">\n        <textarea name=\"description\" rows=\"4\" class=\"span6\" placeholder=\"100 words or less, go easy on the marketspeak please\">";
+  buffer += escapeExpression(stack1) + "\"/>\n      </div>      \n    </div>\n    <div class=\"control-group\">\n      <label class=\"control-label\" for=\"description\">Description</label>\n      <div class=\"controls\">\n        <textarea name=\"description\" rows=\"4\" class=\"span6\" placeholder=\"100 words or less, go easy on the marketspeak or you'll be edited. Explain what your app does.\">";
   foundHelper = helpers.description;
   stack1 = foundHelper || depth0.description;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
